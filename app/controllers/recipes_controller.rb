f2928a20066399484c4da9868a5c29900de30bdc
Minhaz <minhaz725@gmail.com>
2020-10-15 18:10:01 +0600

    basic recipes fetched

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
   def index
+  	@search_term = params[:looking_for] || 'chocolate'#params[:looking_for] || 'cat in the hat'
+    @recipes = Recipe.for(@search_term)
   end
 end
