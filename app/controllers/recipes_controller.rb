31b7929aa8b06bb9660db7342fe38ff51f59cf72
Minhaz <minhaz725@gmail.com>
2020-10-15 20:33:24 +0600

    passed all test

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-  	@search_term = params[:looking_for] || 'chocolate'#params[:looking_for] || 'cat in the hat'
+  	@search_term = params[:search] || 'chocolate'#params[:looking_for] || 'cat in the hat'
     @recipes = Recipe.for(@search_term)
   end
 end
